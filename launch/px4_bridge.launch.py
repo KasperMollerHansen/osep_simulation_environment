7011eba2b79df6db0565f3808925ce529aa21254
KasperMollerHansen <Kasperhansen1998@hotmail.com>
2025-08-21 14:29:46 +0200

    updated launch

--- launch/px4_bridge.launch.py
+++ launch/px4_bridge.launch.py
@@ -1,6 +1,15 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+TOPIC_NAMES = {
+    "VEL_CMD": '/osep/vel_cmd',
+    "PATH": '/osep/path',
+    "COSTMAP": '/osep/local_costmap/costmap',
+    "VIEWPOINTS": '/osep/viewpoints',
+    "VIEWPOINTS_ADJUSTED": '/osep/viewpoints_adjusted',
+    "GROUND_TRUTH": '/osep/ground_truth'
+}
+
 def generate_launch_description():
     return LaunchDescription([
         Node(
@@ -8,7 +17,7 @@ def generate_launch_description():
             executable='px4_msg_converter_node',
             name='px4_msg_converter_node',
             parameters=[{
-                'osep_vel_cmd': '/osep/vel_cmd'
+                'osep_vel_cmd': TOPIC_NAMES["VEL_CMD"]
             }]
         ),
         Node(
@@ -16,9 +25,9 @@ def generate_launch_description():
             executable='px4_vel_controller',
             name='px4_vel_controller',
             parameters=[{
-                'path_topic': '/planner/smoothed_path',
-                'osep_vel_cmd': '/osep/vel_cmd',
-                'interpolation_distance': 2.0,  
+                'path_topic': TOPIC_NAMES["PATH"],
+                'osep_vel_cmd': TOPIC_NAMES["VEL_CMD"],
+                'interpolation_distance': 2.0,
                 'max_speed': 15.0,
                 'inspection_speed': 2.0,
                 'max_yaw_to_velocity_angle_deg': 120.0,
